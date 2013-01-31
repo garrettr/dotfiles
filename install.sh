e0d0b7902db164cc7b91ded02e967ecded13e776
Garrett Robinson <garrett.f.robinson@gmail.com>
2013-01-31 14:40:03 -0800

    Forgot `cat` in install script

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # bash
 cp bash_aliases ~/.bash_aliases
-bash_rc >> ~/.bashrc                # Don't clobber what the distro provides
+cat bash_rc >> ~/.bashrc            # Don't clobber what the distro provides
 
 # git
 cp gitconfig ~/.gitconfig
