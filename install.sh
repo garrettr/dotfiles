72d6faafb823b74042490823ce98b781088bd0dc
Garrett Robinson <garrettr@riseup.net>
2013-09-18 11:59:30 -0700

    update setup script
    * auto-install git and tmux if not found
    * git clone vundle, remove need to store (and periodically update) in this repo
    * style

--- install.sh
+++ install.sh
@@ -2,20 +2,27 @@
 
 # bash
 cp bash_aliases ~/.bash_aliases
-cat bashrc >> ~/.bashrc            # Don't clobber what the distro provides
+cat bashrc >> ~/.bashrc # don't clobber what the distro provides
 echo "Don't forget to source ~/.bashrc."
 
 # git
+# install git if it's not installed (need it to clone vundle later)
+command -v git > /dev/null 2>&1 || \
+    { echo "git not installed, running 'sudo apt-get install git'"; \
+      sudo apt-get install git; }
 cp gitconfig ~/.gitconfig
 
 # screen
 cp screenrc ~/.screenrc
 
 # tmux
+command -v tmux >/dev/null 2>&1 || \
+    { echo "tmux not installed, running 'sudo apt-get install tmux'"; \
+      sudo apt-get install tmux; }
 cp tmux.conf ~/.tmux.conf
 
 # vim
 cp vimrc ~/.vimrc
-mkdir -p ~/.vim
-cp -r vim/* ~/.vim/
+mkdir -p ~/.vim/bundle
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim +BundleInstall +qall
