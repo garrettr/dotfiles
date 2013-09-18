169795d82a3c994faa93a1d770e3002bc1728bd7
Garrett Robinson <garrettr@riseup.net>
2013-09-18 12:16:02 -0700

    auto-install terminus font

--- install.sh
+++ install.sh
@@ -26,3 +26,8 @@ cp vimrc ~/.vimrc
 mkdir -p ~/.vim/bundle
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim +BundleInstall +qall
+
+# terminus
+dpkg -s xfonts-terminus >/dev/null 2>&1 || \
+    { echo "xfonts-terminus not installed, running 'sudo apt-get install xfonts-terminus'";
+      sudo apt-get install xfonts-terminus; }
